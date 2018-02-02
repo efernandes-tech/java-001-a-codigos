ccda5b8b5700119352df25a7d38e1b8b9236d3ec
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-02-01 23:03:19 -0200

    A1 utilizando o tipo int

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,14 @@
+public class TestaVariaveis{
+
+    public static void main(String [] args){
+
+        System.out.println("ola novo teste");
+        
+        int idade;
+        idade = 37;
+        
+        System.out.println("a idade é "+ idade + ", parabéns!");
+        
+    }
+    
+}
