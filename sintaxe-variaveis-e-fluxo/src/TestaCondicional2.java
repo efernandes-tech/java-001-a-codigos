914244c6eba975860a14bd6459f217826ce707a0
edersonlrf <edersonluis.rf@gmail.com>
2018-02-04 09:58:25 -0200

    A6 um pouco mais de if

--- sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -1,16 +1,18 @@
 public class TestaCondicional2 {
 
-        public static void main(String[] args){
-            System.out.println("testando condicionais");
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
 
-            int idade = 16;
-            int quantidadePessoas = 3;        
+		int idade = 16;
+		int quantidadePessoas = 3;
+		boolean acompanhado = quantidadePessoas >= 2;
 
-            if (idade >=18 || quantidadePessoas >=2){
-                System.out.println("Seja bem vindo"); 
-            } else {
-                System.out.println("infelizmente voce nao pode entrar");
-        }
+		if (idade >= 18 && acompanhado) {
 
-    }
+			System.out.println("Seja bem vindo");
+		} else {
+			System.out.println("infelizmente voce nao pode entrar");
+		}
+
+	}
 }
